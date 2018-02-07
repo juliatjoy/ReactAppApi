b656d92d0c40d3d3d09ba9db5c93776a6a8e195b
AiswaryaLakshmy <aiswarya@7nodes.com>
2018-02-07 14:59:41 +0530

    seed added for admin and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u = User.new
+u.email = 'react_admin@7nodes.com'
+u.role = 'admin'
+u.first_name = 'admin'
+u.last_name = 'react'
+u.contact = '1234567890'
+u.password = '123qwe'
+u.password_confirmation = '123qwe'
+u.save
+puts 'Created Admin'
+
+u = User.new
+u.email = 'react_user@7nodes.com'
+u.role = 'user'
+u.first_name = 'user'
+u.last_name = 'react'
+u.contact = '1234567890'
+u.password = '123qwe'
+u.password_confirmation = '123qwe'
+u.save
+puts 'Created User'
